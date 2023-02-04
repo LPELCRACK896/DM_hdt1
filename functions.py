eef9067d5cf07875a4f63a9fdc94058e386fb863
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-02-03 23:06:24 -0600

    last commit

---
+++ functions.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+from constants import DATA_PATH
+import re
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import PolynomialFeatures
+
+def formatt_start_time_into_float(start_time: str):
+    """Transform string values of column formatted: 'Start Time: HH: MM (p.m.|a.m.) Local' and turns into float number in this interval [0, 24)
+
+    Args:
+        start_time (str): String fomatted likes this 'Start Time: HH: MM (p.m.|a.m.) Local'
+
+    Returns:
+        float: Hour turned into a float in this range [0, 24)
+    """
+    if not re.match(r"^\s*(Start Time:)\s?\d?\d:\d\d\s*((p.m.?)|(a.m.?))\s*Local$", start_time): return None
+    
+    start_time = start_time.split(" ")
+    hour_min = start_time[2].split(":")
+    hour = (int(hour_min[0])%12+12) if "p.m" in start_time[3] else int(hour_min[0])%12
+    return hour+int(hour_min[1])/60
+
+def formatt_game_duration_into_float(game_duration: str):
+    """Transforms a string value from column game_duration formatted ': H:MM' into a number in this interval [0, inf.)
+
+    Args:
+        game_duration (str): A string value with this formatted like this ': H:MM'
+
+    Returns:
+        float: Hour turned into a float in this range [0, inf.)
+    """
+    if not re.match(r"^\s*:?\s*\d+:\d+\s*$", game_duration): return None
+
+    game_duration = game_duration.split(":")
+    return (int(game_duration[-1])/60)+int(game_duration[-2])
+
+
+
+def data_cleaner():
+    df = pd.read_csv(DATA_PATH)
+    df.to_excel("baseball_reference_2016_scrape.xlsx", index=False)
+    n_rows = df.shape[0]
+
+    df.drop(columns=["other_info_string", "boxscore_url", "field_type", "date"], inplace=True)
+
+    # Cleaning dataset
+    df['attendance'] = df['attendance'].apply(lambda x: int(x.split("'")[0].replace(",", "")) if (x.split("'")[0].replace(",", "").isnumeric()) else None)
+
+    # Removing data with no info of the attendance
+    df = df.dropna(subset=["attendance"])
+    n_rows = n_rows - df.shape[0]
+    print(f'Rows removed because of null values: {n_rows}')
+
+    df['start_time'] = df['start_time'].apply(formatt_start_time_into_float)
+    df['game_duration'] = df['game_duration'].apply(formatt_game_duration_into_float)
+
+    pattern = re.compile(r'^[^\d]+$')
+    df["venue"] = df["venue"].apply(lambda s: None if not pattern.match(s) else s.replace(":", "").replace(" ", "").lower()) 
+
+    df[["time_condition", "field_conditions"]] = df['game_type'].str.split(',', expand=True)
+    df.drop(columns=["game_type"], inplace=True)
+    return df
+
+
+
+
+def codif_y_ligar(dataframe_original, variables_a_codificar):
+    dummies = pd.get_dummies(dataframe_original[[variables_a_codificar]], prefix=variables_a_codificar)
+    res = pd.concat([dataframe_original, dummies], axis = 1)
+    res = res.drop([variables_a_codificar], axis = 1)
+    return res 
--- main.py
+++ main.py
@@ -1,127 +1,72 @@
+from functions import data_cleaner, codif_y_ligar
+from pandas_profiling import ProfileReport
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.covariance import EllipticEnvelope
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from constants import DATA_PATH
-import re
-import matplotlib.pyplot as plt
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
-from pandas_profiling import ProfileReport
-
-def formatt_start_time_into_float(start_time: str):
-    """Transform string values of column formatted: 'Start Time: HH: MM (p.m.|a.m.) Local' and turns into float number in this interval [0, 24)
-
-    Args:
-        start_time (str): String fomatted likes this 'Start Time: HH: MM (p.m.|a.m.) Local'
-
-    Returns:
-        float: Hour turned into a float in this range [0, 24)
-    """
-    if not re.match(r"^\s*(Start Time:)\s?\d?\d:\d\d\s*((p.m.?)|(a.m.?))\s*Local$", start_time): return None
-    
-    start_time = start_time.split(" ")
-    hour_min = start_time[2].split(":")
-    hour = (int(hour_min[0])%12+12) if "p.m" in start_time[3] else int(hour_min[0])%12
-    return hour+int(hour_min[1])/60
-
-def formatt_game_duration_into_float(game_duration: str):
-    """Transforms a string value from column game_duration formatted ': H:MM' into a number in this interval [0, inf.)
-
-    Args:
-        game_duration (str): A string value with this formatted like this ': H:MM'
-
-    Returns:
-        float: Hour turned into a float in this range [0, inf.)
-    """
-    if not re.match(r"^\s*:?\s*\d+:\d+\s*$", game_duration): return None
-
-    game_duration = game_duration.split(":")
-    return (int(game_duration[-1])/60)+int(game_duration[-2])
-
-
-def codif_y_ligar(dataframe_original, variables_a_codificar):
-    dummies = pd.get_dummies(dataframe_original[[variables_a_codificar]], prefix=variables_a_codificar)
-    res = pd.concat([dataframe_original, dummies], axis = 1)
-    res = res.drop([variables_a_codificar], axis = 1)
-    return res 
-
-def data_cleaner():
-    df = pd.read_csv(DATA_PATH)
-    n_rows = df.shape[0]
-
-    df.drop(columns=["other_info_string", "boxscore_url", "field_type", "date"], inplace=True)
-
-    # Cleaning dataset
-    df['attendance'] = df['attendance'].apply(lambda x: int(x.split("'")[0].replace(",", "")) if (x.split("'")[0].replace(",", "").isnumeric()) else None)
-
-    # Removing data with no info of the attendance
-    df = df.dropna(subset=["attendance"])
-    n_rows = n_rows - df.shape[0]
-    print(f'Rows removed because of null values: {n_rows}')
-
-    df['start_time'] = df['start_time'].apply(formatt_start_time_into_float)
-    df['game_duration'] = df['game_duration'].apply(formatt_game_duration_into_float)
-
-    pattern = re.compile(r'^[^\d]+$')
-    df["venue"] = df["venue"].apply(lambda s: None if not pattern.match(s) else s.replace(":", "").replace(" ", "").lower()) 
-
-    df[["time_condition", "field_conditions"]] = df['game_type'].str.split(',', expand=True)
-    df.drop(columns=["game_type"], inplace=True)
-    return df
 
 def main():
         
     df = data_cleaner()
+    rs = input("¿Desea generar nuevamente el Profile report?(S/N)\n")
+    if rs.lower()=="s":
+        profile = ProfileReport(df)
+        profile.to_file("output.html")
+        input("\n\n\nContinuar??")
+
+    df = df[['attendance', 'field_conditions', 'venue', 'home_team', 'away_team' ]]
 
     # trabajar con los datos
     X = df.iloc[:, 1:]
     Y = df.iloc[:, 0]
-    profile = ProfileReport(df)
-    profile.to_file("output.html")
-    # codificacion de variables
-    variables_a_codificar = ["home_team", "venue", "time_condition", "field_conditions"]
+    # Codificacion de variables
+    variables_a_codificar = ["home_team", "venue", "field_conditions", "away_team"]
     for variable in variables_a_codificar:
         X = codif_y_ligar(X, variable)
-
     # dividir el conjunto de datos para prueba y entrenamiento
-    X_entreno, X_prueba, y_entreno, y_prueba = train_test_split(X, Y, test_size = 0.2, random_state = 1)
+    X_entreno, X_prueba, y_entreno, y_prueba = train_test_split(X, Y, test_size = 0.1, random_state = 1)
 
+    #Elimina datos atipicos
+    outlier_detector = EllipticEnvelope(contamination=0.1)
+    outlier_detector.fit(X_entreno.assign(target=y_entreno))
+    outlier_mask = outlier_detector.predict(X_prueba.assign(target=y_prueba)) == 1
+
+    X_test_inliers = X_prueba[outlier_mask]
+    y_test_inliers = y_prueba[outlier_mask]
+
+    # Fit a linear regression model to the inliers in the training set
+    reg  = LinearRegression() 
+    reg.fit(X_entreno[outlier_detector.predict(X_entreno.assign(target=y_entreno)) == 1],
+                                y_entreno[outlier_detector.predict(X_entreno.assign(target=y_entreno)) == 1])
+
+    rcuadrado = reg.score(X_test_inliers, y_test_inliers)
     #entrenar el modelo de regresion lineal multiple
     poly_regresor = LinearRegression()
     # obtener predicciones de la regresion lineal multiple
-    poly_regresor.fit(X_entreno, y_entreno)
+    # poly_regresor.fit(X_entreno, y_entreno)
 
     #Obtener R cuadrado con los datos de prueba
-    r_squared = poly_regresor.score(X_prueba, y_prueba)
-    print("R^2:", r_squared)
+    # r_squared = poly_regresor.score(X_prueba, y_prueba)
 
-    predicciones = poly_regresor.predict(X_entreno)
+    predicciones = reg.predict(X_entreno)
 
     # Crea la gráfica de la regresión lineal múltiple
     plt.scatter(predicciones, y_entreno)
     plt.xlabel('Predicciones')
     plt.ylabel('Valores reales')
     plt.title('Gráfica de la regresión lineal múltiple')
-    plt.show()
+    #plt.show()
+    #regresor = LinearRegression()
+    #regresor.fit(X_entreno, y_entreno)
+    print(f"\n\n\nR^2: {rcuadrado}")
+    choosen_team = pd.read_excel("./data/choosen_teams.xlsx")
+    pd.concat([X, Y], axis = 1).to_excel("./data/data_excel.xlsx", index=False)
 
-    #Parece que la regresion lineal no es la adecuada :p
-    #polinomial
-    regresor_poli = PolynomialFeatures(degree = 2)
-    X_poli = regresor_poli.fit_transform(X_entreno)
-
-    # obtener predicciones de la regresion lineal multiple
-    regresor = LinearRegression()
-    regresor.fit(X_poli, y_entreno)
-    predicciones = regresor.predict(X_poli)
-
-    #Obtener R cuadrado con los datos de prueba
-    r_squared = regresor.score(X_prueba, y_prueba)
-    print("R^2:", r_squared)
-    
-    
-    #df.to_excel("./data/data_excel.xlsx", index=False)
+    y_pred = reg.predict(choosen_team)
+    print(f"Predicción [Houston Astros v. Cleveleand Indians]: {y_pred[0]}")
+    print(f"Coeficientes: {reg.coef_}")
 
 if __name__== "__main__":
     main()
